b9fa0b19130184ed19dfe5f568e016d8c7074105
Your Name <mikechiang2003@yahoo.com>
2015-08-05 16:39:02 +0100

    add scaling to data_load

--- datasetup.py
+++ datasetup.py
@@ -4,17 +4,20 @@
 import pandas as pd
 import sklearn.cross_validation
 import sklearn.preprocessing
-
 import config
+from sklearn import preprocessing
 
 
 def load_data(fn):
     print(fn)
     df = pd.read_csv(fn)
-    label = df['label']
-    features = df.iloc[:, 4:19]
-    features_scaled = sklearn.preprocessing.scale(features.values)
+    label=df['label']
+    features=df.iloc[:,4:19]
+    min_max_scaler = preprocessing.MinMaxScaler()
+    features_scaled = min_max_scaler.fit_transform(features)
+    #features_scaled = preprocessing.scale(features.values)
     return label.values, features_scaled
+
 
 
 def split_train_data(y):  # TODO control with config, move to runner / add value with real df
