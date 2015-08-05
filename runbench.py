51ff6c42d675ab1cc52502d97bb1ab61741c3951
Ryan Varley <ryanjvarley@gmail.com>
2015-08-05 12:05:20 +0100

    removed unnecessary if statement in runbench handling a non-standard syntax classifier

--- runbench.py
+++ runbench.py
@@ -26,9 +26,6 @@ for train_index, test_index in stratifiedShuffledData:
         print("Running {}".format(clf_name))
         clf.fit(train_features, train_labels)
         
-        if hasattr(clf, "predict_proba"):
-            prob_pos = clf.predict_proba(test_features)[:, 1]
-        else:  # use decision function
-            prob_pos = clf.decision_function(test_features)
+        prob_pos = clf.predict_proba(test_features)[:, 1]
 
         benchmarks.all_benchmarks(prob_pos, test_labels, clf_name, out_path)
