3d000d84263d53d665034870930ac05d059687af
Ryan Varley <ryanjvarley@gmail.com>
2015-08-05 18:30:09 +0100

    Separated results per stratified sample, merging / comparison step needed

--- runbench.py
+++ runbench.py
@@ -8,24 +8,25 @@ out_path = config.config['out_path']
 if not os.path.isdir(out_path):
     os.makedirs(out_path)
 
-# df = load_data(config['data_file'])
-
 # TODO turn df into train_labels, features and test
 
 labels, features = datasetup.load_data(config.config['data_file'])
 
 stratified_shuffled_data = datasetup.split_train_data(labels)
 
-for train_index, test_index in stratified_shuffled_data:
+# TODO move seperation step out of loop and iterate over steps
+# TODO functionalise
+for i, (train_index, test_index) in enumerate(stratified_shuffled_data):
     train_features = features[train_index]
     train_labels = labels[train_index]
     test_features = features[test_index]
     test_labels = labels[test_index]
 
     for clf_name, clf in config.classifiers.iteritems():
-        print("Running {}".format(clf_name))
+        clf_i_name = '{} {}'.format(clf_name, i)
+        print("Running {} sample {}".format(clf_i_name, i))
         clf.fit(train_features, train_labels)
         
-        prob_pos = clf.predict_proba(test_features)[:, 1]
+        prob_pos = clf.predict_proba(test_features)[:, 1]  # TODO standardise this variable
 
-        benchmarks.all_benchmarks(prob_pos, test_labels, clf_name, out_path)
+        benchmarks.all_benchmarks(prob_pos, test_labels, clf_i_name, out_path)
