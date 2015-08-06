560c6f90506f0419f29d2d23687a9b3acbaf2617
Luis C Reyes <lcreyes2001@yahoo.com>
2015-08-06 16:36:15 +0100

    new voya_config.py that goes with previous commit

--- voya_config.py
+++ voya_config.py
@@ -9,7 +9,7 @@ import sklearn.ensemble
 config = {
     "data_file": "test_data.csv",
     "out_path": "output",
-    "num_folds":5,
+    "num_folds":2,
     "test_size": 0.2,
     "random_seed": 49,  # currently only for shuffle
 }
@@ -21,7 +21,17 @@ rbm_logistic = sklearn.pipeline.Pipeline(steps=[('rbm', rbm), ('logistic', logis
 
 classifiers = {
     'lr': sklearn.linear_model.LogisticRegression(),
-    'rfc': sklearn.ensemble.RandomForestClassifier(n_estimators=100),
-    'svc': sklearn.svm.SVC(C=1.0, probability=True),
-    'rbm_logistic': rbm_logistic
+#    'rfc': sklearn.ensemble.RandomForestClassifier(n_estimators=100),
+#    'svc': sklearn.svm.SVC(C=1.0, probability=True),
+#    'rbm_logistic': rbm_logistic
 }
+
+classifiers_gridparameters = {
+    'lr': {'C': [1, 10, 100, 1000],'penalty' : ["l1","l2"], 'class_weight' : [None,"auto"]},
+    'rfc': {"max_depth": [3, None],
+              "max_features": [1, 3, 10],
+              "min_samples_split": [1, 3, 10],
+              "min_samples_leaf": [1, 3, 10],
+              "bootstrap": [True, False],
+              "criterion": ["gini", "entropy"]},
+     }
