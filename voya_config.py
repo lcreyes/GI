96aed376d53ead217ea728f3e4a20bbcb95e6398
Ryan Varley <ryanjvarley@gmail.com>
2015-08-11 11:19:17 +0100

    reactivated BernoulliRBM Logistic without grid search

--- voya_config.py
+++ voya_config.py
@@ -13,25 +13,24 @@ config = {
     "test_size": 0.2,
 }
 
-# Setup BernoulliRBM Neural net with logistic classifiers
+# Setup BernoulliRBM Neural net with logistic classifier
 logistic = sklearn.linear_model.LogisticRegression()
-rbm = sklearn.neural_network.BernoulliRBM(verbose=True)
+rbm = sklearn.neural_network.BernoulliRBM()
 rbm_logistic = sklearn.pipeline.Pipeline(steps=[('rbm', rbm), ('logistic', logistic)])
 
 classifiers = {
-    'Logistic Regression': sklearn.linear_model.LogisticRegression(),
-    # 'Random Forests': sklearn.ensemble.RandomForestClassifier(n_estimators=100),
-    'SVC': sklearn.svm.SVC(C=1.0, probability=True),
-    # 'rbm_logistic': rbm_logistic,
-    # 'Gradient Boosting': sklearn.ensemble.GradientBoostingClassifier(),
+    # 'Logistic Regression': sklearn.linear_model.LogisticRegression(),
+    'Random Forests': sklearn.ensemble.RandomForestClassifier(n_estimators=100),
+    # 'SVC': sklearn.svm.SVC(C=1.0, probability=True),
+    'BernoulliRBM Logistic': rbm_logistic,
+    'Gradient Boosting': sklearn.ensemble.GradientBoostingClassifier(),
 }
 
 classifiers_gridparameters = {
     'Logistic Regression': {'C': [0.2, 0.4, 0.6, 0.8, 1.0], 'penalty': ["l1", "l2"], 'class_weight': [None, "auto"]},
     'Random Forests': {"max_depth": [3, None], "max_features": [1, 3, 10], "min_samples_split": [1, 3, 10],
             "min_samples_leaf": [1, 3, 10], "bootstrap": [True, False], "criterion": ["gini", "entropy"]},
-    # 'SVC': [{'kernel': ['linear'], 'C': [0.2, 0.4, 0.6, 0.8, 1.0]}],
-    'SVC': None,
-    # 'rbm_logistic' : {'steps' : [('rbm', rbm), ('logistic', logistic)]},
+    # 'SVC': [{'kernel':['linear'], 'C': [0.2, 0.4, 0.6, 0.8, 1.0]}],
+    'BernoulliRBM Logistic' : None,
     'Gradient Boosting': {"n_estimators": [10, 50, 100], 'learning_rate': [0.1, 0.2, 0.3, 0.5], 'max_depth': [1, 2, 5],}
 }
