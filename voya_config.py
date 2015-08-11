cb2b56ba4e29ab6d6196c900c4a019ccc724bc6f
Luis C Reyes <lcreyes2001@yahoo.com>
2015-08-11 12:30:52 +0100

    testing with Gaussian data

--- voya_config.py
+++ voya_config.py
@@ -7,7 +7,7 @@ import sklearn.pipeline
 import sklearn.ensemble
 
 config = {
-    "data_file": "data/test_data_formatted.csv",
+    "data_file": "data/test_Gaussians_formatted.csv",
     "out_path": "output",
     "num_folds": 2,
     "test_size": 0.2,
@@ -20,18 +20,18 @@ rbm_logistic = sklearn.pipeline.Pipeline(steps=[('rbm', rbm), ('logistic', logis
 
 classifiers = {
     'Logistic Regression': sklearn.linear_model.LogisticRegression(),
-    # 'Random Forests': sklearn.ensemble.RandomForestClassifier(n_estimators=100),
+    'Random Forests': sklearn.ensemble.RandomForestClassifier(n_estimators=100),
     'SVC': sklearn.svm.SVC(C=1.0, probability=True),
-    # 'rbm_logistic': rbm_logistic,
-    # 'Gradient Boosting': sklearn.ensemble.GradientBoostingClassifier(),
+    'BernoulliRBM Logistic': rbm_logistic,
+    'Gradient Boosting': sklearn.ensemble.GradientBoostingClassifier(),
+
 }
 
 classifiers_gridparameters = {
     'Logistic Regression': {'C': [0.2, 0.4, 0.6, 0.8, 1.0], 'penalty': ["l1", "l2"], 'class_weight': [None, "auto"]},
     'Random Forests': {"max_depth": [3, None], "max_features": [1, 3, None, 'auto'], "min_samples_split": [1, 2, 3],
             "min_samples_leaf": [1, 2, 3, 4], "bootstrap": [True, False], "criterion": ["gini", "entropy"]},
-    # 'SVC': [{'kernel': ['linear'], 'C': [0.2, 0.4, 0.6, 0.8, 1.0]}],
-    'SVC': None,
-    # 'rbm_logistic' : {'steps' : [('rbm', rbm), ('logistic', logistic)]},
+     'SVC': [{'kernel': ['linear', 'rbf'], 'C': [0.2, 0.4, 0.6, 0.8, 1.0]}],
+    'BernoulliRBM Logistic' : None,
     'Gradient Boosting': {"n_estimators": [10, 50, 100], 'learning_rate': [0.1, 0.2, 0.3, 0.5], 'max_depth': [1, 2, 5],}
 }
