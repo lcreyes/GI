46aeacc57587222aeba37a607876fbce26c1a31f
Mauricio Cisternas <mauricio.cisternas@yahoo.com>
2015-08-12 10:18:47 +0100

    style

--- voya_plotter.py
+++ voya_plotter.py
@@ -116,12 +116,12 @@ def roc_curve(y_test, y_pred, clf_name):
     #roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
 
     # Plot of a ROC curve for a specific class
-    seaborn.set_style("white")
-    plt.figure(figsize=(7, 7))
+    seaborn.set_style("whitegrid")
+    plt.figure(figsize=(6, 6))
     plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
     plt.plot([0, 1], [0, 1], 'k--')
     plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
+    plt.ylim([0.0, 1.0])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
     plt.title('Receiver operating characteristic')
