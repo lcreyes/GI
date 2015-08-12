18cb865881b2a76523acb45aaaf8f56668b80dd7
Luis C Reyes <lcreyes2001@yahoo.com>
2015-08-12 13:23:59 +0100

    adding bagging LR method (just in case, since it's easy to implement)

--- voya_config_example_pu.py
+++ voya_config_example_pu.py
@@ -4,6 +4,7 @@ See description in voya_config_example for more information
 """
 
 import sklearn.svm
+import sklearn.linear_model
 import sklearn.ensemble
 
 from puLearning.puAdapter import PUAdapter
@@ -16,15 +17,24 @@ config = {
     "pu_learning": True,
 }
 
-svc_estimator = sklearn.svm.SVC(C=10, kernel='rbf', gamma=0.4, probability=True)
+#best parameters for rbf kernel(according to GridSearch)
+#svc_estimator = sklearn.svm.SVC(C=2.5, kernel='rbf', gamma=0.2, class_weight='auto')
+#best parameter for linear kernel (according to GridSearch)
+svc_estimator = sklearn.svm.SVC(C=2.5, kernel='linear', class_weight='auto', probability=True)
+
+#best parameters for LR (according to GridSearch)
+LR_estimator = sklearn.linear_model.LogisticRegression(C=0.4, penalty='l1')
 
 
 classifiers = {
     'puestimator': PUAdapter(svc_estimator, hold_out_ratio=0.2),
     'Bagging SVC': sklearn.ensemble.BaggingClassifier(svc_estimator),
+    'Bagging LR': sklearn.ensemble.BaggingClassifier(LR_estimator),
 }
 
 classifiers_gridparameters = {
    'puestimator': None,
-   'Bagging SVC': {'n_estimators': [50, 100], 'max_samples': [0.1, 0.3]},
+   'Bagging SVC': {'n_estimators': [100, 200, 300], 'max_samples': [0.1, 0.3, 0.5, 0.7]},
+   'Bagging LR': {'n_estimators':[100, 200, 300], 'max_samples':[0.1, 0.3, 0.5, 0.7]},
+
 }
