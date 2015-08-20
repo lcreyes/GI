41fbe3834f3764b44b5aa961c849dbee5079ad7e
mikechiang2003 <mikechiang2003@yahoo.com>
2015-08-20 11:04:44 +0100

    inherit scikit base for gridsearch

--- pulearning.py
+++ pulearning.py
@@ -274,7 +274,7 @@ class PULearnByDoubleWeighting(BaseEstimator, TransformerMixin):
 
         return self.estimator.predict(X)
         
-class PUBagging(sklearn.ensemble.BaggingClassifier):
+class PUBagging(sklearn.ensemble.BaggingClassifier,BaseEstimator, TransformerMixin):
     """
     Runs the bagging approach suggested by Mordelet & Vert (2010), namely:
 
